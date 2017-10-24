a52597992229019561aea831491de7c807c39157
Bart Veneman <bartveneman@gmail.com>
2017-10-24 22:46:49 +0200

    Reduce LOC for color analysis
    * Loop over the color regexes instead of going over them one by
      one manually
    * Fix broken test where `whitesmoke` wasn't recognised as color
    * Add extra test to make sure that colors of different types
      are found in a single value

--- src/utils/css.js
+++ src/utils/css.js
@@ -28,8 +28,10 @@ module.exports.color = {
   ],
 
   // Source: https://github.com/tiaanduplessis/colors-regex/blob/master/index.js
+  // Colornames are reversed to make sure that 'white' is matched before
+  // 'whitesmoke' etc.
   regex: {
-    name: new RegExp(`${colorNameDelimiter}(${Object.keys(colorNames).join('|')})${colorNameDelimiter}`, 'gi'),
+    name: new RegExp(`${colorNameDelimiter}(${Object.keys(colorNames).reverse().join('|')})${colorNameDelimiter}`, 'gi'),
     hex: /#([a-f0-9]{8}|[a-f0-9]{6}|[a-f0-9]{4}|[a-f0-9]{3})\b/gi,
     rgb: /rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d*(?:\.\d+)?)\)|rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)/gi,
     hsl: /hsl\(\s*(\d+)\s*,\s*(\d*(?:\.\d+)?%)\s*,\s*(\d*(?:\.\d+)?%)\)|hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%,\s*(\d*(?:\.\d+)?)\)/gi
