a2cb63615a5753f15372bfd6e1edf3009957e7f1
Bart Veneman <bartveneman@gmail.com>
2018-03-06 15:10:06 +0100

    Adds a parser test for invalid CSS

--- test/parser/index.js
+++ test/parser/index.js
@@ -2,25 +2,37 @@ const test = require('ava')
 const parser = require('../../src/parser')
 
 test('parser returns a Promise', t => {
-  t.true(parser('') instanceof Promise)
+  t.true(parser('a{}') instanceof Promise)
 })
 
 test('parser returns a list of @rules', async t => {
-  const actual = await parser('')
+  const actual = await parser('a{}')
   t.true(Array.isArray(actual.atRules))
 })
 
 test('parser returns a list of rules', async t => {
-  const actual = await parser('')
+  const actual = await parser('a{}')
   t.true(Array.isArray(actual.rules))
 })
 
 test('parser returns a list of selectors', async t => {
-  const actual = await parser('')
+  const actual = await parser('a{}')
   t.true(Array.isArray(actual.selectors))
 })
 
 test('parser returns a list of declarations', async t => {
-  const actual = await parser('')
+  const actual = await parser('a{}')
   t.true(Array.isArray(actual.declarations))
+})
+
+test('parser handles incorrect css', async t => {
+  // Notice the missing ; after the first declaration
+  // It is intentional to trigger an error
+  const cssWithSyntaxError = `
+    a {
+      font-size: 16px
+      background: red;
+    }`
+
+  await t.throws(parser(cssWithSyntaxError))
 })
