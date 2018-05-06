8561558376b7580d0e7e2816db648f8c34ea0443
Bart Veneman <bartveneman@users.noreply.github.com>
2018-05-06 14:51:53 +0200

    Ignore declarations in @font-face rules, since they are technically descriptors instead

--- src/parser/declarations.js
+++ src/parser/declarations.js
@@ -1,7 +1,12 @@
 module.exports = tree => {
   const declarations = []
+  const IGNORE_FROM_PARENT = 'font-face'
 
   tree.walkDecls(declaration => {
+    if (declaration.parent.name === IGNORE_FROM_PARENT) {
+      return
+    }
+
     declarations.push({
       property: declaration.prop,
       value: declaration.value,
