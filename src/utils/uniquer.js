3bf31e5f7d28cf7eafe90e35a3d0b368c2beca02
Bart Veneman <bartveneman@users.noreply.github.com>
2018-12-21 23:35:51 +0100

    update color-sorter to v3.0.0

--- src/utils/uniquer.js
+++ src/utils/uniquer.js
@@ -3,23 +3,22 @@ const stringSortFn = require('string-natural-compare')
 module.exports = (values, sortFn) => {
   sortFn = sortFn || stringSortFn.caseInsensitive
 
-  const reduced = [...values.reduce((map, value) => {
-    // Create a Map of unique values and their counts
-    return map.set(value, map.get(value) + 1 || 1)
-  }, new Map())].map(value => {
+  // Create a Map of unique values and their counts
+  const reduced = [
+    ...values.reduce((map, value) => {
+      return map.set(value, map.get(value) + 1 || 1)
+    }, new Map())
+  ]
     // Create an array of [{value, count}]
-    return {
-      value: value[0],
-      count: value[1]
-    }
-  })
+    .map(value => {
+      return {
+        value: value[0],
+        count: value[1]
+      }
+    })
 
-  const sorted = reduced
-    .map(el => el.value)
-    .sort(sortFn)
-
-  const unique = sorted
-    .map(value => reduced.find(r => r.value === value))
+  const sorted = reduced.map(el => el.value).sort(sortFn)
+  const unique = sorted.map(value => reduced.find(r => r.value === value))
 
   return {
     unique,
