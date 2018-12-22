1ce75d5a62e746965b410a412ed08e0aa786ea62
Bart Veneman <bartveneman@users.noreply.github.com>
2018-12-22 16:34:43 +0100

    add max identifier count analysis
    it's easier to get the max identifier count from a simple object than havingto get it from an array

--- src/analyzer/selectors/identifiers.js
+++ src/analyzer/selectors/identifiers.js
@@ -1,37 +1,42 @@
 const specificity = require('specificity')
 const {caseInsensitive: stringCompare} = require('string-natural-compare')
 
+// Sort by identifiers count, then by alphabet
+function sortByIdentifiersCount(a, b) {
+  if (a.count === b.count) {
+    return stringCompare(a, b)
+  }
+
+  return b.count - a.count
+}
+
+function getSelectorSpecificity(selector) {
+  return specificity.calculate(selector).shift()
+}
+
 module.exports = selectors => {
-  const totalSelectors = selectors.length
   const identifiersPerSelector = selectors
-    .map(selector => specificity.calculate(selector).shift())
-    .map(selector => {
+    .map(getSelectorSpecificity)
+    .map(specificity => {
       return {
-        selector: selector.selector,
-        identifiers: selector.parts.length
+        value: specificity.selector,
+        count: specificity.parts.length
       }
     })
 
   const totalIdentifiers = identifiersPerSelector
-    .map(selector => selector.identifiers)
+    .map(selector => selector.count)
     .reduce((prev, curr) => prev + curr, 0)
+
+  const totalSelectors = selectors.length
   const average = totalIdentifiers / totalSelectors
 
-  const top = count => {
-    // Sort by identifiers count, then by alphabet
-    const sorter = (a, b) => {
-      if (a.identifiers === b.identifiers) {
-        return stringCompare(a, b)
-      }
-
-      return b.identifiers - a.identifiers
-    }
-
-    return identifiersPerSelector.sort(sorter).slice(0, count)
-  }
+  const sorted = identifiersPerSelector.sort(sortByIdentifiersCount)
+  const [max] = sorted
 
   return {
     average,
-    top: top(5)
+    max,
+    top: sorted.slice(0, 5)
   }
 }
