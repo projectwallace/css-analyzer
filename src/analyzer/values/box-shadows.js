5fc504e54c2d956aab89e4d71bdac575b167c018
Bart Veneman <bartveneman@users.noreply.github.com>
2019-02-04 20:06:55 +0100

    ignore keywords for box-shadows

--- src/analyzer/values/box-shadows.js
+++ src/analyzer/values/box-shadows.js
@@ -1,8 +1,10 @@
 const uniquer = require('../../utils/uniquer')
+const {KEYWORDS} = require('../../utils/css')
 
 module.exports = declarations => {
   const all = declarations
     .filter(({property}) => property === 'box-shadow')
+    .filter(({value}) => !KEYWORDS.includes(value))
     .map(declaration => declaration.value)
 
   const {unique, totalUnique} = uniquer(all)
