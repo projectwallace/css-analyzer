bb0e03bb3f0b98a09146ad7afc0ac88f585e0c97
Bart Veneman <bartveneman@users.noreply.github.com>
2019-03-03 23:05:28 +0100

    improve tests for fonts

--- src/analyzer/values/font-families.js
+++ src/analyzer/values/font-families.js
@@ -3,23 +3,22 @@ const uniquer = require('../../utils/uniquer')
 const {KEYWORDS} = require('../../utils/css')
 
 module.exports = declarations => {
-  const all = declarations
-    .reduce((prev, {property, value}) => {
-      if (property === 'font-family') {
-        prev = [...prev, value]
-      }
-
-      if (property === 'font') {
-        const expanded = expand('font', value)
-
-        if (expanded && expanded['font-family']) {
-          prev = [...prev, expanded['font-family']]
-        }
-      }
-
+  const all = declarations.reduce((prev, {property, value}) => {
+    if (KEYWORDS.includes(value)) {
       return prev
-    }, [])
-    .filter(value => !KEYWORDS.includes(value))
+    }
+
+    if (property === 'font-family') {
+      prev = [...prev, value]
+    }
+
+    if (property === 'font') {
+      const {'font-family': family} = expand('font', value)
+      prev = [...prev, family]
+    }
+
+    return prev
+  }, [])
 
   return {
     total: all.length,
