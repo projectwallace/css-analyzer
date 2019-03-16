c092085a83fdda603d84e39c44f52f42ae6949c1
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-03-16 22:33:54 +0100

    rewrite tests for properties (#113)

--- test/analyzer/properties/index.js
+++ test/analyzer/properties/index.js
@@ -1,9 +1,60 @@
 const test = require('ava')
-const testScope = require('../../utils/scope-tester.js')
+const analyze = require('../../../src/analyzer/properties')
 
-const SCOPE = 'properties'
+test('it responds with the correct structure', t => {
+  const actual = analyze([])
+  const expected = {
+    total: 0,
+    totalUnique: 0,
+    unique: [],
+    browserhacks: {
+      total: 0,
+      totalUnique: 0,
+      unique: []
+    },
+    prefixed: {
+      total: 0,
+      totalUnique: 0,
+      unique: [],
+      share: 0
+    }
+  }
 
-test(SCOPE, async t => {
-  const {actual, expected} = await testScope(SCOPE)
-  t.deepEqual(actual[SCOPE], expected)
+  t.deepEqual(actual, expected)
+})
+
+test('it counts all properties', t => {
+  const fixture = [
+    {property: 'color', value: 'unset'},
+    {property: 'border', value: 'unset'},
+    {property: 'font-size', value: 'unset'}
+  ]
+  const {total: actual} = analyze(fixture)
+  const expected = 3
+
+  t.is(actual, expected)
+})
+
+test('it lists all unique properties with their count and sorted alphabeticlly', t => {
+  const fixture = [
+    {property: 'color', value: 'unset'},
+    {property: 'color', value: 'unset'},
+    {property: 'border', value: 'unset'}
+  ]
+  const {unique: actual} = analyze(fixture)
+  const expected = [{value: 'border', count: 1}, {value: 'color', count: 2}]
+
+  t.deepEqual(actual, expected)
+})
+
+test('it counts all unique properties', t => {
+  const fixture = [
+    {property: 'color', value: 'unset'},
+    {property: 'color', value: 'unset'},
+    {property: 'border', value: 'unset'}
+  ]
+  const {totalUnique: actual} = analyze(fixture)
+  const expected = 2
+
+  t.is(actual, expected)
 })
