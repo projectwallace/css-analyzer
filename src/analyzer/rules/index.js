8cf1d0dfdba8ead90c875c7d1327e549f7f8f39e
Bart Veneman <bartveneman@users.noreply.github.com>
2019-03-28 22:32:34 +0100

    add selectors per rule metric

--- src/analyzer/rules/index.js
+++ src/analyzer/rules/index.js
@@ -6,10 +6,20 @@ module.exports = rules => {
   const all = rules
   const empty = rules.filter(isEmpty)
 
+  const selectorsPerRule = rules.map(({selectorsCount}) => selectorsCount)
+
   return {
     total: all.length,
     empty: {
       total: empty.length
+    },
+    selectors: {
+      min: selectorsPerRule.length > 0 ? Math.min(...selectorsPerRule) : 0,
+      max: selectorsPerRule.length > 0 ? Math.max(...selectorsPerRule) : 0,
+      average:
+        selectorsPerRule.length > 0
+          ? selectorsPerRule.reduce((acc, curr) => acc + curr, 0) / rules.length
+          : 0
     }
   }
 }
--- src/parser/selectors.js
+++ src/parser/selectors.js
@@ -1,20 +1,16 @@
+const {isKeyframes} = require('./atrules')
+
 module.exports = tree => {
   const selectors = []
 
   tree.walkRules(rule => {
     // Don't include the 'selectors' (from, 50%, to, etc.) inside @keyframes
-    if (rule.parent &&
-      rule.parent.type === 'atrule' &&
-      rule.parent.name === 'keyframes') {
+    if (isKeyframes(rule)) {
       return
     }
 
     // Get selectors: flatten the list, split each by ',' and trim the results
-    selectors.push(
-      ...rule.selector
-        .split(',')
-        .map(s => s.trim())
-    )
+    selectors.push(...rule.selector.split(',').map(s => s.trim()))
   })
 
   return selectors
