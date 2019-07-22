6a3c3687355a097441672d525c6ea8a1cf7e66af
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-07-22 21:21:57 +0200

    Adds `text-shadow` analysis (#151)
    Closes #145

---
+++ src/analyzer/values/text-shadows.js
@@ -0,0 +1,17 @@
+const uniquer = require('../../utils/uniquer')
+const {KEYWORDS} = require('../../utils/css')
+
+module.exports = declarations => {
+  const all = declarations
+    .filter(({property}) => property === 'text-shadow')
+    .filter(({value}) => !KEYWORDS.includes(value))
+    .map(declaration => declaration.value)
+
+  const {unique, totalUnique} = uniquer(all)
+
+  return {
+    total: all.length,
+    unique,
+    totalUnique
+  }
+}
