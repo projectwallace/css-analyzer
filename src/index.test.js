8e30379d514282d8e4dcc6e0a8eaac781687a423
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-04-09 12:42:10 +0200

    run test on bundled output

--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,18 @@
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { analyze, compareSpecificity } from './index.js'
-import { readFileSync } from 'fs'
-
-const fixtures = [
-  'bol-com-20190617.css',
-  'css-tricks-20190319.css',
-  'facebook-20190319.css',
-  'gazelle-20210905.css',
-  'github-20210501.css',
-  'lego-20190617.css',
-  'smashing-magazine-20190319.css',
-  'trello-20190617.css',
-].map(fileName => {
-  const css = readFileSync(`./src/__fixtures__/${fileName}`, 'utf-8')
-  return {
-    css,
-    fileName
-  }
-})
+import { build } from 'vite'
+import viteConfig from '../vite.config.cjs'
 
 const Api = suite('Public API')
+
+Api('runs on bundled output', async () => {
+  await build(viteConfig)
+  const { analyze } = await import('../dist/analyzer.es.js')
+  const result = analyze('test{}')
+
+  assert.is(result.selectors.total, 1)
+})
 
 Api('exposes the analyze method', () => {
   assert.is(typeof analyze, 'function')
