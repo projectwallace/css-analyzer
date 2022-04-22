23b88c0cefe9d753d1109ad83deaec1bfe73947c
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-04-22 15:20:32 +0200

    revert fs/promises

--- src/smoke.test.js
+++ src/smoke.test.js
@@ -1,47 +1,40 @@
-import * as fs from 'fs/promises'
+import * as fs from 'fs'
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { analyze } from './index.js'
 
 const Smoke = suite('Smoke testing')
 
-const fixtures = {
+Object.entries({
   'Bol.com': 'bol-com-20190617',
   'Bootstrap v5.0.0': 'bootstrap-5.0.0',
   'CNN': 'cnn-20220403',
   'CSS Tricks': 'css-tricks-20190319',
+  'CNN': 'cnn-20220403',
   'Facebook': 'facebook-20190319',
   'GitHub': 'github-20210501',
   'Lego.com': 'lego-20190617',
   'Trello': 'trello-20190617',
   'Gazelle': 'gazelle-20210905',
   'Smashing Magazine': 'smashing-magazine-20190319',
-}
-
-const files = await Promise.all(Object.entries(fixtures).map(([name, fileName]) => {
-  return [
-    fs.readFile(`./src/__fixtures__/${fileName}.json`, 'utf-8'),
-    fs.readFile(`./src/__fixtures__/${fileName}.css`, 'utf-8'),
+}).map(([name, fileName]) => {
+  const css = fs.readFileSync(`./src/__fixtures__/${fileName}.css`, 'utf-8')
+  const json = fs.readFileSync(`./src/__fixtures__/${fileName}.json`, 'utf-8')
+  return {
     name,
     fileName,
-  ]
-}).flat())
-
-for (let i = 0; i < files.length; i += 4) {
-  const json = files[i]
-  const css = files[i + 1]
-  const name = files[i + 2]
-  const fileName = files[i + 3]
-
+    json,
+    css,
+  }
+}).forEach(({ name, fileName, css, json }) => {
   // const result = analyze(css)
   // delete result.__meta__
-  // await fs.writeFile(`./src/__fixtures__/${fileName}.json`, JSON.stringify(result, null, 2))
-
+  // fs.writeFileSync(`./src/__fixtures__/${fileName}.json`, JSON.stringify(result, null, 2))
   Smoke(`${name} matches fixture`, () => {
     const result = analyze(css)
     delete result.__meta__
     assert.fixture(JSON.stringify(result, null, 2), json)
   })
-}
+})
 
 Smoke.run()
