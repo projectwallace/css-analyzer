0c5b3ea66d4083f395e027194c96ffdb05b8fb65
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-04-24 12:10:14 +0200

    reduce memory usage for OccurrenceCounter by hashing the values

--- src/occurrence-counter.js
+++ src/occurrence-counter.js
@@ -1,14 +1,28 @@
+function hash(str) {
+  let hash = 0x811c9dc5
+  var prime = 0x000193
+
+  for (let i = 0; i < str.length; i++) {
+    hash = hash ^ str.charCodeAt(i)
+    hash *= prime
+  }
+
+  return hash
+}
+
 export class OccurrenceCounter {
   constructor() {
     this._items = Object.create(null)
   }
 
   push(item) {
-    if (this._items[item]) {
-      return this._items[item]++
+    const key = hash(item)
+
+    if (this._items[key]) {
+      return this._items[key]++
     }
 
-    return this._items[item] = 1
+    return this._items[key] = 1
   }
 
   count() {
