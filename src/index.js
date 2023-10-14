7eeca7071e499d9b6a965d3526cabcc211331a00
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2023-10-14 15:42:50 +0200

    revert Condition parsing

--- src/index.js
+++ src/index.js
@@ -187,6 +187,16 @@ export function analyze(css, options = {}) {
           }
           break
         }
+        if (atRuleName === 'supports') {
+          let prelude = node.prelude
+          let condition = stringifyNode(prelude)
+          supports.push(condition, prelude.loc)
+
+          if (isSupportsBrowserhack(prelude)) {
+            supportsBrowserhacks.push(condition, prelude.loc)
+          }
+          break
+        }
         if (endsWith('keyframes', atRuleName)) {
           let name = '@' + atRuleName + ' ' + stringifyNode(node.prelude)
           if (hasVendorPrefix(atRuleName)) {
@@ -216,21 +226,6 @@ export function analyze(css, options = {}) {
       }
       case 'Layer': {
         layers.push(node.name, node.loc)
-        break
-      }
-      case 'Condition': {
-        if (node.kind === 'supports') {
-          if (node.children && node.children.size === 1 && node.children.first.type === 'Condition') {
-            break
-          }
-          let prelude = stringifyNode(node)
-          supports.push(prelude, node.loc)
-
-          if (isSupportsBrowserhack(node)) {
-            supportsBrowserhacks.push(prelude, node.loc)
-          }
-          return this.skip
-        }
         break
       }
       case 'Rule': {
