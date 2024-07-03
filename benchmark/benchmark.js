015f19d5470f2c86254ac16830a9a316fda1a005
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2024-07-03 19:14:43 +0200

    only benchmark 2 CSS files for faster CI

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -4,12 +4,18 @@ import * as fs from "fs"
 import { analyze } from '../src/index.js'
 
 let filelist = fs.readdirSync('./src/__fixtures__')
+let files = filelist
+  .filter(filename => filename.endsWith('.css'))
+  .filter(filename => filename.includes('github') || filename.includes('css-tricks'))
+  .reduce((acc, filename) => {
+    let css = fs.readFileSync(`./src/__fixtures__/${filename}`, 'utf8')
+    acc.set(filename, css)
+    return acc
+  }, new Map())
 
 let bench = withCodSpeed(new Bench())
 
-for (let filename of filelist) {
-  if (!filename.endsWith('.css')) continue
-  let css = fs.readFileSync(`./src/__fixtures__/${filename}`, 'utf8')
+for (let [filename, css] of files) {
   let byte_size = (Buffer.byteLength(css) / 1024).toFixed(1)
   bench.add(`${filename} (${byte_size}kB)`, () => analyze(css))
 }
