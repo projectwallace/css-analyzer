36cb58777e99d5ae097579ec8d2d3bdf6e3b5ad0
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-06-13 23:02:16 +0200

    feat: add analysis for spacing resets (#467)
    Prior art: csssstats.com
    
    This analysis checks for:
    
    - scientific notation (`0e0`)
    - signed numbers (`+0`, `-0`)
    - floats (`0.0`)
    - with or without units (`0`, `0em`)
    - vendor prefixed property names (`-webkit-padding-inline`)
    - all of the values in a shorthand being zero (`0 0 0 0`, `0px 0 0em`)
    - combinations of the above (`-moz-margin: 0 0px +0.0e0em`)
    
    Implementation sparked by @ohhelloana

--- src/values/values.js
+++ src/values/values.js
@@ -1,5 +1,5 @@
 import { KeywordSet } from "../keyword-set.js"
-import { Identifier } from "../css-tree-node-types.js"
+import { Identifier, Value, Nr, Dimension } from "../css-tree-node-types.js"
 
 export const keywords = new KeywordSet([
   'auto',
@@ -23,4 +23,26 @@ export function isValueKeyword(node) {
 
   let firstChild = children.first
   return firstChild.type === Identifier && keywords.has(firstChild.name)
+}
+
+/**
+ * @param {string} string
+ * @returns {boolean}
+ */
+function isZero(string) {
+  return parseFloat(string) === 0
+}
+
+/**
+ * Test whether a value is a reset (0, 0px, -0.0e0 etc.)
+ * @param {import('css-tree').Value} node
+ */
+export function isValueReset(node) {
+  for (let child of node.children.toArray()) {
+    if (child.type === Nr && isZero(child.value)) continue
+    if (child.type === Dimension && isZero(child.value)) continue
+    return false
+  }
+
+  return true
 }
