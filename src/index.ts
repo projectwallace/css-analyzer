f49c8d4250bc8d4aa792eb3d3b0c1d77dd690fc0
Bart Veneman <bartveneman@gmail.com>
2026-01-19 18:49:06 +0100

    rm comment

--- src/index.ts
+++ src/index.ts
@@ -313,8 +313,6 @@ function analyzeInternal<T extends boolean>(css: string, options: Options, useLo
 		} else if (node.type === STYLE_RULE) {
 			// Handle keyframe rules specially
 			if (inKeyframes && node.prelude) {
-				// In keyframes, the prelude is a SelectorList that may not have Selector children
-				// (e.g., "50%" is just a SelectorList with text, no Selector child)
 				if (node.prelude.type === SELECTOR_LIST && node.prelude.children.length > 0) {
 					for (let keyframe_selector of node.prelude.children) {
 						keyframeSelectors.p(keyframe_selector.text, toLoc(keyframe_selector))
