44adad3fccd1db8d16e85cdc7479aa21abe92617
Bart Veneman <bartveneman@gmail.com>
2026-02-07 17:40:04 +0100

    add test to ensure that !IMPORTANT is not marked as browserhack

--- src/values/browserhacks.test.ts
+++ src/values/browserhacks.test.ts
@@ -3,7 +3,7 @@ import { expect } from 'vitest'
 import { analyze } from '../index.js'
 
 test('finds hacks', () => {
-  const fixture = `
+	const fixture = `
     value-browserhacks {
       property: value !ie;
       property: value !test;
@@ -11,36 +11,37 @@ test('finds hacks', () => {
       property: value\\9;
     }
   `
-  const actual = analyze(fixture).values.browserhacks
-  const expected = {
-    total: 4,
-    totalUnique: 4,
-    unique: {
-      'value !ie': 1,
-      'value !test': 1,
-      'value!nospace': 1,
-      "value\\9": 1,
-    },
-    uniquenessRatio: 4 / 4
-  }
-  expect(actual).toEqual(expected)
+	const actual = analyze(fixture).values.browserhacks
+	const expected = {
+		total: 4,
+		totalUnique: 4,
+		unique: {
+			'value !ie': 1,
+			'value !test': 1,
+			'value!nospace': 1,
+			'value\\9': 1,
+		},
+		uniquenessRatio: 4 / 4,
+	}
+	expect(actual).toEqual(expected)
 })
 
 test('reports no false positives', () => {
-  const fixture = `
+	const fixture = `
     value-browserhacks {
       property: value !important;
       content: '!important';
+      margin: 0 !IMPORTANT;
+      margin: 0 !important;
       aspect-ratio: 16/9;
     }
   `
-  const actual = analyze(fixture).values.browserhacks
-  const expected = {
-    total: 0,
-    totalUnique: 0,
-    unique: {},
-    uniquenessRatio: 0,
-  }
-  expect(actual).toEqual(expected)
+	const actual = analyze(fixture).values.browserhacks
+	const expected = {
+		total: 0,
+		totalUnique: 0,
+		unique: {},
+		uniquenessRatio: 0,
+	}
+	expect(actual).toEqual(expected)
 })
-
