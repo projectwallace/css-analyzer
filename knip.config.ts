739f5bca56f4c74e585388ae6b467f38b5dc0200
Bart Veneman <bartveneman@gmail.com>
2026-05-13 11:41:43 +0200

    fix lint and knip

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,6 @@ import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
 	project: ['src/**/*.ts'],
-	ignoreDependencies: ['prettier', '@vitest/coverage-v8'],
 }
 
 export default config
